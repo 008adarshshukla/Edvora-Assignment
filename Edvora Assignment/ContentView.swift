0920f359a71395ea583bbb5e8b48c482a3887c72
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2022-08-31 22:28:24 +0530

    Initial Commit

---
+++ Edvora Assignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Edvora Assignment
+//
+//  Created by Adarsh Shukla on 31/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Edvora Assignment/Edvora_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edvora_AssignmentApp.swift
+//  Edvora Assignment
+//
+//  Created by Adarsh Shukla on 31/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Edvora_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
